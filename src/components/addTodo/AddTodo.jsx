021bf7216190f90e0002ed9fe27147709c469b29
ChiaraJF <chiara.fell87@gmail.com>
2024-04-26 20:02:56 +0200

    backend terminato

---
+++ src/components/addTodo/AddTodo.jsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.scss";
+
+const AddTodo = () => {
+  return <button className={styles.button}>Add todo</button>;
+};
+
+export default AddTodo;
---
+++ src/pages/api/todos/[id].js
@@ -0,0 +1,64 @@
+import dbConnection from "../../../../utils/dbConnection";
+import Todo from "../../../models/Todo";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  const { id } = req.query;
+
+  try {
+    await dbConnection();
+
+    switch (method) {
+      case "GET":
+        try {
+          const todoItem = await Todo.findById(id);
+          if (!todoItem) {
+            return res
+              .status(404)
+              .json({ success: false, error: "Todo Item not Found" });
+          }
+          res.status(200).json({ success: true, data: todoItem });
+        } catch (error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+        break;
+
+      case "PUT":
+        try {
+          const body = req.body;
+          const todoItem = await Todo.findByIdAndUpdate(id, body, {
+            new: true,
+          });
+          if (!todoItem) {
+            return res
+              .status(404)
+              .json({ success: false, error: "Todo item not found" });
+          }
+          res.status(200).json({ success: true, data: todoItem });
+        } catch (error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+        break;
+
+      case "DELETE":
+        try {
+          const todoItem = await Todo.findByIdAndDelete(id);
+          if (!todoItem) {
+            return res
+              .status(404)
+              .json({ success: false, error: "Todo item not found" });
+          }
+          res.status(200).json({ success: true, data: [] });
+        } catch (error) {
+          res.status(400).json({ success: false, error: error.message });
+        }
+        break;
+
+      default:
+        res.setHeaders("Allow", ["GET", "PUT", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } catch (error) {
+    res.status(400).json({ success: false, error: error.message });
+  }
+}
